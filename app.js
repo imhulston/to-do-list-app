187ace6b5fbbb31540d9e731608a4bad44710b50
Ian Hulston <mihulston@gmail.com>
2018-08-22 22:33:54 +0100

    Added removeItem and called the function with saveList to the first code block.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function addItem() {
 	var newItem = document.createElement("div");
 	newItem.innerHTML = document.getElementById("box").value;
+	newItem.onclick = removeItem;
 	document.getElementById("list").appendChild(newItem);
+	saveList();
 }
 
 function removeItem() {
